826a58f72dd79278512fe04d88759ca6473d7ebf
Shivam Gohel <shivgohel13@gmail.com>
2025-04-16 12:52:57 +0530

    edited TC in better approach

--- Sliding Window/LongestRepeatingCharacterReplacement.java
+++ Sliding Window/LongestRepeatingCharacterReplacement.java
@@ -71,10 +71,10 @@ class LongestRepeatingCharacterReplacement
      - Track the frequency of characters within the window using a HashMap.
      - If the window is valid (i.e., the number of characters needing replacement does not exceed `k`), expand the window by moving the `right` pointer.
      - If the window becomes invalid (i.e., we need more than `k` replacements), move the `left` pointer to shrink the window.
-     - recalculate the maxiumum_frequency as well 
+     - recalculate the maxiumum_frequency as well. TC:O(26)
      - Keep updating the maximum length of valid windows found.
       
-     Time Complexity: O(n) — we only pass through the string once.
+     Time Complexity: O(n * 26) — we only pass through the string once.
      Space Complexity: O(k) — HashMap stores frequencies of characters in the window.
     */
     private static int longestRepeatingCharacterReplacementBetter(String s, int k){
